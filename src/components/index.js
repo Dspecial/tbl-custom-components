abd33f284aa8918fecc473c8b27ea7c580d6e394
Dspecial <1214302256@qq.com>
2023-02-06 17:18:53 +0800

    国际化

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@
  * @Author: dxx
  * @Date: 2022-12-13 16:11:52
  * @LastEditors: dxx
- * @LastEditTime: 2023-02-06 14:31:40
+ * @LastEditTime: 2023-02-06 16:39:49
  */
 // 定义 install 方法，接受 Vue 作为参数，如果使用 use 注册插件，则所有的组件都将被注册
 // 导入组件：按需导入时导入文件夹下的index.js文件， 全局导入时导入 .vue文件或者index.js文件都可以
@@ -27,6 +27,7 @@ const install = function (Vue, options={}) {
   if (options && options.components) {
     components = options.components;
   }
+
   locale.use(options.locale);
   locale.i18n(options.i18n);
 
