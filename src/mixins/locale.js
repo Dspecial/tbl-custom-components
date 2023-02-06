e4a14af3eb67b2572e4e51ee8e1b05904edcf2a6
Dspecial <1214302256@qq.com>
2023-02-06 18:05:34 +0800

    国际化

--- src/mixins/locale.js
+++ src/mixins/locale.js
@@ -1,28 +1,9 @@
-// import { t } from '../locale/index';
+import { t } from '../locale/index';
 
-// export default {
-//   methods: {
-//     t(...args) {
-//       return t.apply(this, args);
-//     }
-//   }
-// };
-
-
-'use strict';
-
-exports.__esModule = true;
-
-var _locale = require('../locale/index');
-
-exports.default = {
+export default {
   methods: {
-    t: function t() {
-      for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-        args[_key] = arguments[_key];
-      }
-
-      return _locale.t.apply(this, args);
+    t(...args) {
+      return t.apply(this, args);
     }
   }
 };
